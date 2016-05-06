1951b85e9f2247052c92d1f5a81f98a4278f9435
雷卷 <jacky.chenlb@alibaba-inc.com>
2016-05-06 14:31:11 +0800

    ok http client properties

---
+++ src/main/java/org/mvnsearch/spring/boot/okhttp3/OkHttp3Properties.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.spring.boot.okhttp3;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * ok http client properties
+ *
+ * @author linux_china
+ */
+/*@ConfigurationProperties(
+        prefix = "spring.okhttp3"
+)*/
+public class OkHttp3Properties {
+}
